bdc6b5d5a66b2dae88f426c66c7513a0fe16814d
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-29 13:04:43 +0800

    Update __init__.py

--- wcloud/__init__.py
+++ wcloud/__init__.py
@@ -1,4 +1,5 @@
 from .server import createServer
 from .run import WcloudRunner
+from .client import connectServer
 
-__all__ = ["createServer","WcloudRunner"]
+__all__ = ["createServer","WcloudRunner","connectServer"]
