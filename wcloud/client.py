8d6e9130f68682c504dc7182e63aad3f441b910d
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-29 13:40:20 +0800

    Update client.py

--- wcloud/client.py
+++ wcloud/client.py
@@ -13,10 +13,10 @@ class connectServer:
             for container in containers:
                 if container.ports.get(f"{self.port}/tcp"):
                     return container
-            print(f"No running container found on port {self.port}")
+            print(f"此端口沒有分機連接，端口： {self.port}")
             return None
         except docker.errors.DockerException as e:
-            print(f"Error connecting to container: {e}")
+            print(f"連接分機錯誤: {e}")
             return None
 
     def run_command(self, cmds: str):
@@ -25,10 +25,10 @@ class connectServer:
                 result = self.container.exec_run(cmds)
                 return result
             else:
-                print("No container connected")
+                print("沒有連接分機")
                 return None
         except docker.errors.DockerException as e:
-            print(f"Error running command: {e}")
+            print(f"運行指令錯誤: {e}")
             return None
 
     def upload(self, local_file_path, remote_file_path):
@@ -39,7 +39,7 @@ class connectServer:
             else:
                 print("No container connected")
         except (IOError, docker.errors.DockerException) as e:
-            print(f"Error uploading file: {e}")
+            print(f"上傳檔案錯誤: {e}")
 
     def download(self, remote_file_path, local_file_path):
         try:
@@ -51,7 +51,7 @@ class connectServer:
             else:
                 print("No container connected")
         except (IOError, docker.errors.DockerException) as e:
-            print(f"Error downloading file: {e}")
+            print(f"下載檔案錯誤: {e}")
     def stop_container(self):
         if self.container:
             self.container.stop()
