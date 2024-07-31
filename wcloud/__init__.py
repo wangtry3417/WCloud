702ffecab122c1c45facac9859af3e18f18fb8e3
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-31 15:04:57 +0800

    Update __init__.py

--- wcloud/__init__.py
+++ wcloud/__init__.py
@@ -1,5 +1,6 @@
 from .server import createServer
 from .run import WcloudRunner
 from .client import connectServer
+from .install import check_docker_env
 
-__all__ = ["createServer","WcloudRunner","connectServer"]
+__all__ = ["createServer","WcloudRunner","connectServer","check_docker_env"]
